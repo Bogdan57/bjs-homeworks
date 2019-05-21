44329c71ef87b86f969e300192aed493f0707987
Bogdan Novikov <fikis57@mail.ru>
2019-05-21 22:02:28 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,15 @@
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let result = algebra + geography + physics;
+console.log(result)
+console.log()
+let name = "Bogdan";
+let massage = `Привет, мир! Меня зовут ${name}`;
+console.log(massage)
+console.log()
+let x = 2;
+let y = 22;
+let z = 0;
+let result2 = x * y + 5 * z + x - 1;
+console.log(result2)
